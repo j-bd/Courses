68c9387d430dcfc12ba2f767e24482aaa9c4ec56
j-bd <recodata.out@gmail.com>
2020-04-11 18:09:44 +0200

    preprocessing

--- python/ml_commands.py
+++ python/ml_commands.py
@@ -13,6 +13,51 @@ import seaborn as sns
 
 df = pd.read_csv("path")
 
+
+
+# =============================================================================
+# Global overview
+# =============================================================================
+df.describe()
+df.info()
+df.columns
+df.isnull().sum(axis=0)
+df.head()
+df.nunique(dropna = False) #nb unique value for each column
+
+
+
+# =============================================================================
+# OUTLIERS
+# =============================================================================
+
+" Catégoriser les outliers de la population du label ==> Proposer un petit script"
+" pour visualiser les outliers sur la variable cible."
+fig, axes = plt.subplots(nrows=2,ncols=2)
+fig.set_size_inches(12, 10)
+sns.boxplot(data=df,y="rented",orient="v",ax=axes[0][0])
+sns.boxplot(data=df,y="rented",x="season",orient="v",ax=axes[0][1])
+sns.boxplot(data=df,y="rented",x="hour",orient="v",ax=axes[1][0])
+sns.boxplot(data=df,y="rented",x="workingday",orient="v",ax=axes[1][1])
+
+axes[0][0].set(ylabel='rented',title="Box Plot On Count")
+axes[0][1].set(xlabel='Season', ylabel='rented',title="Box Plot On Count Across Season")
+axes[1][0].set(xlabel='Hour Of The Day', ylabel='rented',title="Box Plot On Count Across Hour Of The Day")
+axes[1][1].set(xlabel='Working Day', ylabel='rented',title="Box Plot On Count Across Working Day")
+
+"méthode pour filtrer ces données anormales (Par exemple en utilisant la variance"
+" de la distribution). Combien d'outliers à-t-on filtré ?:"
+
+dailyDataWithoutOutliers = df[
+    np.abs(df["rented"] - df["rented"].mean()) <= (3 * df["rented"].std())
+]
+
+print ("Shape of the dataframe before ouliers removal: ",df.shape)
+print ("Shape of the dataframe after ouliers removal: ",dailyDataWithoutOutliers.shape)
+
+
+
+
 # =============================================================================
 # Graph
 # =============================================================================
@@ -113,3 +158,80 @@ y = loi_normale(x)
 plt.plot(x,y)
 
 
+# =============================================================================
+# MISSING VALUES
+# =============================================================================
+missing_values_pourc = df.isnull().sum()/len(df)*100
+missing_values_pourc[missing_values_pourc>0].sort_values(ascending = False)
+'Drop'
+col_list = [idx for idx, val in missing_values_pourc.iteritems() if val >= 60]
+df = df.drop(columns=col_list)
+'imputer les valeurs manquantes avec le mode'
+data_columns = df.columns
+for col in data_columns:
+    df[col].fillna(df[col].mode()[0],inplace = True)
+
+
+# =============================================================================
+# SELECTION DE VARIABLES
+# =============================================================================
+
+"Calculer la variance pour l'ensemble des variables quantitatives"
+df.var().sort_values(ascending=True)
+"Gardez uniquement les variables avec une variance supérieure ou égale à 10"
+numeric_variables = df.select_dtypes(include=[np.number])
+numeric_variables_variance = numeric_variables.var()
+numeric_columns = numeric_variables.columns
+columns_LVF = [ ]
+for i in range(0,len(numeric_columns)):
+    if numeric_variables_variance[i]>=10 :
+        columns_LVF.append(numeric_columns[i])
+
+import seaborn as sns
+
+"Filtre à corrélation élevée"
+applications_without_target = df.drop('TARGET', 1)
+f, ax = plt.subplots(figsize = (14,12))
+plt.title('Correlation of Features - HeatMap',y=1,size=16)
+sns.heatmap(applications_without_target.corr(),square = True,  vmax=0.8)
+
+
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+"PCA"
+'Convertir le dataframe en tableau (type arrays) puis appliquer PCA à 2 composantes.'
+'df without target variable'
+
+cells_array = df.values
+pca = PCA(n_components=2)
+pca_2d = pca.fit_transform(cells_array)
+
+" Appliquer-t-SNE avec les paramètres suivants : n_components=2, verbose=1, perplexity=40, n_iter=2000"
+tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=2000)
+tsne_2d = tsne.fit_transform(cells_array)
+
+"Visualiser le résultat de t-SNE et de PCA côte-à-côte en mettant la variable cible en couleur"
+plt.figure(figsize = (16,11))
+plt.subplot(121)
+plt.scatter(pca_2d[:,0],pca_2d[:,1], c = cells_target,
+            cmap = "coolwarm", edgecolor = "None", alpha=0.35)
+plt.colorbar()
+plt.title('PCA Scatter Plot')
+
+
+plt.subplot(122)
+
+plt.scatter(tsne_2d[:,0],tsne_2d[:,1],  c = cells_target,
+            cmap = "coolwarm", edgecolor = "None", alpha=0.35)
+plt.colorbar()
+plt.title('TSNE Scatter Plot')
+plt.show()
+
+"Standardiser l'ensemble des variables"
+cells_array_std = StandardScaler().fit_transform(cells_array)
+pca = PCA(n_components=2)
+pca_2d_std = pca.fit_transform(cells_array_std)
+
+
+tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=2000)
+tsne_2d_std = tsne.fit_transform(cells_array_std)
