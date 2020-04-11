eaaea5ae615e6f47a1dec76afd88608160033544
j-bd <recodata.out@gmail.com>
2020-04-11 19:13:09 +0200

    estimation

--- python/ml_commands.py
+++ python/ml_commands.py
@@ -228,6 +228,7 @@ plt.title('TSNE Scatter Plot')
 plt.show()
 
 "Standardiser l'ensemble des variables"
+from sklearn.preprocessing import StandardScaler
 cells_array_std = StandardScaler().fit_transform(cells_array)
 pca = PCA(n_components=2)
 pca_2d_std = pca.fit_transform(cells_array_std)
@@ -235,3 +236,86 @@ pca_2d_std = pca.fit_transform(cells_array_std)
 
 tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=2000)
 tsne_2d_std = tsne.fit_transform(cells_array_std)
+
+
+
+# =============================================================================
+# Exemple
+# =============================================================================
+from sklearn.datasets import make_classification
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+
+# generate 2 class dataset
+X, y = make_classification(n_samples=1000, n_classes=2, weights=[1,1], random_state=1)
+
+# split into train/test sets
+trainX, testX, trainy, testy = train_test_split(X, y, test_size=0.5, random_state=2)
+# fit a model
+model = KNeighborsClassifier(n_neighbors=3)
+model.fit(trainX, trainy)
+
+# predict probabilities
+probs = model.predict_proba(testX)
+
+# keep probabilities for the positive outcome only
+probs = probs[:, 1]
+
+
+
+# =============================================================================
+# Evaluation de l algorithme
+# =============================================================================
+from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_auc_score
+from matplotlib import pyplot
+
+
+'to compute the AUC and plot the ROC curve '
+# calculate AUC
+model_auc = roc_auc_score(testy, probs)
+print('AUC: %.3f' % model_auc)
+# calculate roc curve
+fpr, tpr, thresholds = roc_curve(testy, probs)
+# plot no skill
+pyplot.plot([0, 1], [0, 1], linestyle='--')
+# plot the roc curve for the model
+pyplot.plot(fpr, tpr, marker='.')
+# show the plot
+pyplot.show()
+
+
+
+"""
+Precision Recall curve
+F1, AUC and AP scores
+skilful models are generally above 0.5.
+"""
+from sklearn.metrics import auc
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import f1_score
+from sklearn.metrics import average_precision_score
+
+# predict probabilities
+probs = model.predict_proba(testX)
+# keep probabilities for the positive outcome only
+probs = probs[:, 1]
+# predict class values
+yhat = model.predict(testX)
+# calculate precision-recall curve
+precision, recall, thresholds = precision_recall_curve(testy, probs)
+# calculate F1 score
+f1 = f1_score(testy, yhat)
+# calculate precision-recall AUC !!!Different from ROC-AUC!!!
+pr_auc = auc(recall, precision)
+# calculate average precision score
+ap = average_precision_score(testy, probs)
+print('f1=%.3f auc=%.3f ap=%.3f' % (f1, pr_auc, ap))
+# plot no skill
+pyplot.plot([0, 1], [0.5, 0.5], linestyle='--')
+# plot the precision-recall curve for the model
+pyplot.plot(recall, precision, marker='.')
+# show the plot
+pyplot.show()
+
+
